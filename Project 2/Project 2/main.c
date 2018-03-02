44f1aab40b319fd35409f808b45a4d2b3f6d94b1
ewapaci <ewapac@onet.pl>
2018-03-02 18:57:46 +0100

    costam

--- Project 2/Project 2/main.c
+++ Project 2/Project 2/main.c
@@ -1,10 +1,29 @@
 #include<stdio.h>
 #include<conio.h>
+#include<Windows.h>
 int main() {
+	float a;
+	float b;
+	float wynik;
+	printf("Podaj pierwsza liczbe rzeczywista: ");
+	scanf_s("%f", &a);
+	printf("Podaj druga liczbe rzeczywista: ");
+	scanf_s("%f", &b);
+	if (b != 0)
+	{
+		wynik = a / b;
+		printf("Wynik dzielenia to %f\n", wynik);
+	}
+	else {
+		printf("Dzielenie przez zero nie yyystnieje");
+		Sleep(3000);
+		return;
+	}
 
-
-
-
-	_getch;
+	_getch();
 	return;
 }
+
+//sgrt("liczba") - pierwiastek
+//math.h - biblioteka z funkcjami matematycznymi
+//#include<Windows.h>  Sleep("iloœæ czasu w milisekundach(1s = 1000milisekund)"); - zatrzymuje okno konsoli na okreslony czas
